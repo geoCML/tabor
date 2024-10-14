c60ac4471d190d7cdd0c3f5a54ac2c7e2df6c04e
Tristan Damron <tristandamron@protonmail.com>
2024-10-14 16:29:53 -0700

    Support boolean data types + ignore raster layers

--- src/tabor_field_type.py
+++ src/tabor_field_type.py
@@ -2,13 +2,17 @@ class TaborFieldType(object):
     postgres_integer_types = ("smallint", "integer", "bigint", "int2", "int4", "int8")
     postgres_numerical_types = ("double precision", "float", "numeric")
     postgres_text_types = ("character varying", "name", "text")
+    postgres_binary_types = ("boolean")
+    postgres_collection_types = ("TEXT ARRAY", "text array")  # TODO: https://github.com/geoCML/tabor/issues/27
+    postgres_generic_types = ("USER-DEFINED", "user-defined")  # TODO: https://github.com/geoCML/tabor/issues/26
 
     def __init__(self, type: str):
-        self.valid_types = ("text", "int", "numeric")
-        self.set_type(type)
+        self.valid_types = ("text", "int", "numeric", "boolean", "text array", "user-defined")
+        if type != "":
+            self.set_type(type)
 
 
-    def convert_postgres_type_to_tabor_field_type(self, type):
+    def convert_postgres_type_to_tabor_field_type(self, type: str) -> str:
         if type in self.postgres_integer_types:
             return "int"
 
@@ -17,6 +21,15 @@ class TaborFieldType(object):
 
         if type in self.postgres_numerical_types:
             return "numeric"
+
+        if type in self.postgres_binary_types:
+            return "boolean"
+
+        if type in self.postgres_collection_types:
+            return type.lower()
+
+        if type in self.postgres_generic_types:
+            return "user-defined"
 
         return type
 
